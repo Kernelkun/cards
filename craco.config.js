c371bacdb0ce3ac267cdc441f8c13870267eba04
José Juan Gázquez <josejuan.gazquez@intelygenz.com>
2020-10-17 20:19:00 +0200

    First commit: Craco + Storybook + Plop + ESLint (airbnb) + Prettier

---
+++ craco.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  babel: {
+    plugins: ['@babel/plugin-proposal-export-default-from']
+  },
+  webpack: {
+    alias: {
+      actions: path.resolve(__dirname, 'src/actions/'),
+      commonConstants: path.resolve(__dirname, 'src/commonConstants/'),
+      components: path.resolve(__dirname, 'src/components/'),
+      contexts: path.resolve(__dirname, 'src/contexts/'),
+      img: path.resolve(__dirname, 'src/img/'),
+      pages: path.resolve(__dirname, 'src/pages/'),
+      reducers: path.resolve(__dirname, 'src/reducers/'),
+      services: path.resolve(__dirname, 'src/services/'),
+      src: path.resolve(__dirname, 'src/'),
+      store: path.resolve(__dirname, 'src/store/'),
+      support: path.resolve(__dirname, 'src/support/'),
+      utils: path.resolve(__dirname, 'src/utils/')
+    }
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^actions(.*)$': '<rootDir>/src/actions/',
+        '^commonConstants(.*)$': '<rootDir>/src/commonConstants/',
+        '^components(.*)$': '<rootDir>/src/components/',
+        '^contexts(.*)$': '<rootDir>/src/contexts/',
+        '^img(.*)$': '<rootDir>/src/img/',
+        '^pages(.*)$': '<rootDir>/src/pages/',
+        '^reducers(.*)$': '<rootDir>/src/reducers/',
+        '^services(.*)$': '<rootDir>/src/services/',
+        '^store(.*)$': '<rootDir>/src/store/',
+        '^support(.*)$': '<rootDir>/src/support/',
+        '^utils(.*)$': '<rootDir>/src/utils/'
+      }
+    }
+  }
+}
---
+++ plopfile.js
@@ -0,0 +1,71 @@
+module.exports = plop => {
+  plop.setGenerator('Component', {
+    description: 'Create a component',
+    // User input prompts provided as arguments to the template
+    prompts: [
+      {
+        // Raw text input
+        type: 'input',
+        // Variable name for this input
+        name: 'name',
+        // Prompt to display on command line
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        // Add a new file
+        type: 'add',
+        // Path for the new file
+        path: 'frontend/src/components/{{pascalCase name}}/index.js',
+        // Handlebars template used to generate content of new file
+        templateFile: 'plop-templates/frontend/components/index.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'frontend/src/components/{{pascalCase name}}/Styles.js',
+        templateFile: 'plop-templates/frontend/components/ComponentStyles.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'frontend/src/components/{{pascalCase name}}/{{pascalCase name}}.stories.js',
+        templateFile: 'plop-templates/frontend/components/ComponentStory.js.hbs'
+      }
+    ]
+  })
+
+  plop.setGenerator('Page', {
+    description: 'Create a page',
+    // User input prompts provided as arguments to the template
+    prompts: [
+      {
+        // Raw text input
+        type: 'input',
+        // Variable name for this input
+        name: 'name',
+        // Prompt to display on command line
+        message: 'What is your page name?'
+      }
+    ],
+    actions: [
+      {
+        // Add a new file
+        type: 'add',
+        // Path for the new file
+        path: 'frontend/src/pages/{{pascalCase name}}/index.js',
+        // Handlebars template used to generate content of new file
+        templateFile: 'plop-templates/frontend/pages/index.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'frontend/src/pages/{{pascalCase name}}/Styles.js',
+        templateFile: 'plop-templates/frontend/pages/PageStyles.js.hbs'
+      },
+      {
+        type: 'add',
+        path: 'frontend/src/pages/{{pascalCase name}}/View.js',
+        templateFile: 'plop-templates/frontend/pages/View.js.hbs'
+      }
+    ]
+  })
+}
